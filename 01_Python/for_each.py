8dd5536094483a358493734a88a060f24786ce8b
Kushagra Sharma <165701787+Kuska3@users.noreply.github.com>
2026-08-21 11:03:53 +0000

    new Concept

---
+++ 01_Python/for_each.py
@@ -0,0 +1,26 @@
+items = [
+    {
+    "name" : "Headphone",
+    "mfg" : "20/01/2019",
+    "brand" : "Boult"
+    },
+    {
+    "name" : "Phone",
+    "mfg" : "20/01/2019",
+    "brand" : "Samsung"
+    },
+    {
+    "name" : "Laptop",
+    "mfg" : "20/01/2019",
+    "brand" : "Alineware"
+    },
+    {
+    "name" : "WiFi",
+    "mfg" : "20/01/2019",
+    "brand" : "Jio"
+    },
+
+]
+
+for item in items:
+    print(item)
